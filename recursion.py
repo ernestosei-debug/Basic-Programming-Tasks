db602632f992f38f98cd31406d0679e298ea9f8a
ernestosei-debug <67836929+ernestosei-debug@users.noreply.github.com>
2021-04-06 18:56:15 -0400

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,7 @@
+def func(x):
+    if(x == 0):
+        print(0)
+    else:
+        print(x)
+        func(x - 1)
+func(10)
---
+++ reverse_string.js
@@ -0,0 +1,19 @@
+//Write a function to reverse a string
+
+//Using built-in functions
+let rev1 = s => {
+    var reverse1 = "";
+    reverse1 = s.split('').reverse().join("");
+    return reverse1;
+}
+console.log(rev1("Ernest"));
+
+//Decrementing for loop
+let rev2 = (s) => {
+    reverse2 = "";
+    for(j = s.length - 1; j >= 0; --j){
+        reverse2 += s[j];
+    }
+    return reverse2;
+}
+console.log(rev2("Ernest"));
