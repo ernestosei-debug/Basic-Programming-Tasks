e5ed00a1d8ea138b8113c9b490861b14eb20e0e8
ernestosei-debug <67836929+ernestosei-debug@users.noreply.github.com>
2021-04-07 19:36:05 -0400

    Add files via upload

---
+++ greatest_value.js
@@ -0,0 +1,13 @@
+//Finding the element with the greatest value
+
+arr = [42, 13, 25, 84, 8];
+
+//Using a built-in functions
+Math.max(...arr);
+sortedArr = arr.sort().pop();
+
+//Using a for loop
+var val = arr[0]
+for(i = 1; i < arr.length; i++){
+    if(val < arr[i]) val = arr[i];
+}
