f28cdd1c1496791bf7b1ce118383e552896eab75
ernestosei-debug <67836929+ernestosei-debug@users.noreply.github.com>
2021-12-01 13:02:56 -0500

    Update greatest_value.js

--- greatest_value.js
+++ greatest_value.js
@@ -6,6 +6,6 @@ arr = [42, 13, 25, 84, 8];
 Math.max(...arr);
 sortedArr = arr.sort().pop();
 
-//Using a for loop
+//using a for loop
 var val = arr[0]
 for(i = 1; i < arr.length; i++) if(val < arr[i]) val = arr[i];
